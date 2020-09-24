261ddd566039ea801ca06a5801fc94a59238cb31
vansoundz <evannivan@gmail.com>
2020-09-24 08:23:25 +0300

    redirects for netlify

--- src/services/util.ts
+++ src/services/util.ts
@@ -2,6 +2,7 @@ import cookies from "js-cookie";
 
 export const setHead = (multipart: boolean = false) => {
   const t = cookies.get(`auth`);
+  console.log(t);
   const headers = {
     "Content-Type": multipart ? "multipart/form-data" : "application/json",
     "moon-auth": t,
